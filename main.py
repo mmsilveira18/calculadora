2eaec8d366838a49326b115feba2bfe6c6ed6f37
Moisés Silveira <98496452+mmsilveira18@users.noreply.github.com>
2022-09-21 10:00:12 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+print('Calculadora')
+print('+ Adição ')
+print('- Subtração')
+print('* Multiplicação')
+print('/ Divisão')
+print('Pressione outra tela para sair')
+
+op = input('Qual operação deseja fazer?')
+if op == '+' or op == '-' or op == '*' or op == '/':
+ x = int(input('Digite o primeiro valor:'))
+ y = int(input('Digite o segundo valor:'))
+
+if (op == '+' ):
+    res = x + y
+    print('Resultado: {} + {} = {}'.format(x, y, res))
+elif (op == '-'):
+    res = x - y
+    print('Resultado: {} - {} = {}'.format(x, y, res))
+elif (op == '*'):
+    res = x * y
+    print('Resultado: {} * {} = {}'.format(x, y, res))
+elif (op == '/'):
+    res = x / y
+    print('Resultado {] / {} = {}'.format(x, y, res))
+else:
+    print('Operação inválida.')
+
+print('Encerrando o programa...')
